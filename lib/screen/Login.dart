6124dcc270d116ce84d78dcb68380c6d0424f83e
hartamatamatama <katonojutsu340@gmail.com>
2025-11-07 09:43:53 +0700

    Membuat TextField Email

--- lib/screen/Login.dart
+++ lib/screen/Login.dart
@@ -27,6 +27,27 @@ class LoginScreen extends StatelessWidget {
                   style: TextStyle(fontSize: 15, color: Colors.deepPurple),
                 ),
                 SizedBox(height: 50),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.email_outlined),
+                    label: Text("Email"),
+                    hint: Text("Masukkan email Anda"),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      borderSide: BorderSide(color: Colors.grey[300]!),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      borderSide: BorderSide(
+                        color: Colors.deepPurple[700]!,
+                        width: 2,
+                      ),
+                    ),
+                    // hintText: "halo",
+                  ),
+                ),
+                SizedBox(height: 20),
               ],
             ),
           ),
