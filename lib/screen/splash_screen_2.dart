a2d3e8b75ef2327f5e680cd4b8144ba75b6b50b9
hartamatamatama <katonojutsu340@gmail.com>
2025-11-07 09:11:57 +0700

    Menambahkan gambar di splash sreen 2

--- lib/screen/splash_screen_2.dart
+++ lib/screen/splash_screen_2.dart
@@ -6,6 +6,30 @@ class SplashScreen2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(),
+            SizedBox(height: 50),
+            Container(
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.deepPurple,
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/images/image2.png'),
+                ),
+              ),
+            ),
+            //tambahkan disini untuk widget widget lainnya nanti
+            SizedBox(height: 30),
+          ],
+        ),
+      ),
+    );
   }
 }
